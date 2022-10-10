f438c093067be5dfa3635fee17c8f2967a860486
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-10-10 16:01:09 +0700

    menambahkan hero

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,4 +1,5 @@
 
     
 </body>
+<?= $this->renderSection('footer') ?>
 </html>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,4 +1,4 @@
-<div class="navbar sticky top-0">
+<div class="navbar fixed top-0 z-30">
     <div class="navbar-start">
         <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
