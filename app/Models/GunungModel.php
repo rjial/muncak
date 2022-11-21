5d77c7d79c2c9caadb723be5ac2dd5e4b67270a3
agungbayyuu <agungbayyuu@gmail.com>
2022-11-21 17:21:34 +0700

    add GunungModel.php

---
+++ app/Models/GunungModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GunungModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'gunung';
+    protected $primaryKey       = 'id_gunung';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['id_gunung' ,'nama','deskripsi','url_gunung','book_available'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
