baea3d195089ca39daa7962352fb223b9fc69ff5
Roy Andani Putra <royandaniputra198728@gmail.com>
2022-12-12 09:14:20 +0700

    Endpoint REST API /api/gunung/add

--- app/Controllers/GunungController.php
+++ app/Controllers/GunungController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Controllers;
-
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
 use App\Models\GunungModel;
@@ -18,37 +17,58 @@ class GunungController extends ResourceController
     use ResponseTrait;
     public function index()
     {
-
-
+       
+       
         $key    = getenv('TOKEN_SECRET');
         $header = $this->request->getServer('HTTP_AUTHORIZATION');
-
+        
         if (!$header) return $this->failUnauthorized('Token Required');
-        $token = explode(' ', $header)[1];
+       $token = explode(' ', $header)[1];
         try {
             $decoded = JWT::decode($token, new Key($key, 'HS256'));
             $model = new GunungModel();
             $data = $model->findAll();
             return $this->respond($data, 200);;
-        } catch (\Throwable $th) {
+        }
+        catch (\Throwable $th){
             return $this->fail('Invalid Token');
         }
-
-        // $response = [
-        //     'status'    => 200,
-        //     'error'     => null,
-        //     'messages'  => [
-        //     'success'   => 'User Login Successfully!'
-        //     ],
-        //     'Token'     => $token
-        // ];
-        // return $this->respond($response);
+        
     }
-
     public function add()
     {
-        helper(['auth']);
+        $key = getenv('TOKEN_SECRET');
+        $header = $this->request->getServer('HTTP_AUTHORIZATION');
+        if (!$header) return $this->failUnauthorized('Token Required');
+       $token = explode(' ', $header)[1];
+       try {
+            $model = new GunungModel();
+            $data = [
+                'nama' => $this->request->getPost('nama'),
+                'deskripsi' => $this->request->getPost('deskripsi'),
+                'url_gunung' => $this->request->getPost('url_gunung'),
+                'book_available' => $this->request->getPost('book_available')
+            ];
+            //$data = json_decode(file_get_contents("php://input"));
+            $response = [
+                'status'   => 201,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Data Saved'
+                ]
+            ];
+             
+            return $this->respond($response);
 
+       }
+       catch (\Throwable $th){
+        return $this->fail('Invalid Token');
+        }
+    }
+    public function create ()
+    {
+        helper(['auth']);
         return view('gunung/addgunung');
     }
+
 }
